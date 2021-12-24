57eb6e6f2db61d7b7cb8780d1028d0770a076315
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2021-12-24 22:05:12 +0530

    e

--- app4.js
+++ app4.js
@@ -156,7 +156,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     //callbackURL: "http://localhost:3000/auth/google/dashboard",
-    callbackURL: "https://warm-ravine-20044.herokuapp.com/auth/google/dashboard",
+    callbackURL: "https://warm-ravine-20044.herokuapp.com/oauth2/google/dashboard",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
 },
 function(accessToken, refreshToken, profile, done) {
